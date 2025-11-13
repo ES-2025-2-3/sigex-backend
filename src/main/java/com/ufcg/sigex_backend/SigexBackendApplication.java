f5c59dac4b5a1cd86711f851c9f3a449df15354b
Arthur Fernandes <arthur.fv324@gmail.com>
2025-11-13 10:45:31 -0300

    feat(project): create the project

---
+++ src/main/java/com/ufcg/sigex_backend/SigexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ufcg.sigex_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigexBackendApplication.class, args);
+	}
+
+}
